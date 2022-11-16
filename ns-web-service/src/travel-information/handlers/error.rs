a201a12c5fa58b6807e5488a322823361f0697a8
Tjardo <dev@tjardo.com>
2022-11-16 11:23:44 +0100

    Return error code 404

--- ns-web-service/src/travel-information/handlers/error.rs
+++ ns-web-service/src/travel-information/handlers/error.rs
@@ -1,5 +1,5 @@
 use actix_web::HttpResponse;
 
 pub async fn error_page_handler() -> HttpResponse {
-    HttpResponse::Ok().json("404 - Page not found")
+    HttpResponse::NotFound().json("404 - Page not found")
 }
